ff76b055a2a2ecc1db5eed384ec1cc4ad9ea075c
davidjosephind <davidjosephind@gmail.com>
2020-11-13 08:16:36 +0530

    Added some behaviour
    Added hover states for appliances in the dropdown list. Also added click functionality for all the appliances on sale.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function onSubmit(){
-    alert("Form has been submitted");
-}
+$(".dropdown-item").click(function(){
+    var itemName = $(this).attr("id");
+    alert("You chose a " + itemName);
+});
