8480d784a05358460fc7cbce6c5b1da34ac4853f
davidjosephind <davidjosephind@gmail.com>
2020-11-13 14:25:04 +0530

    Added price to the dropdown list
    Added price for each item to the dropdown list. Created an array with the product id, name and price. Script adds the items from the list into the webpage.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var cartItems = [];
+var shopItems = [["fridge", "Fridge", 109000], ["desktop", "Desktop PC", 49000], ["laptop", "Laptop", 79000], ["microwave", "Microwave", 19000], ["oven", "Oven", 89000]];
 
-var choosePaymentMethod = false;
+for(var i = 0; i < shopItems.length; i++){
+    $(".dropdownItems").before("<div class='dropdown-item'><span class='appliance' id=" + shopItems[i][0] + ">" + shopItems[i][1] + "</span><span class='price'>" + shopItems[i][2] + "</span></div>");
+}
+
+var cartItems = [];
 
 $(".dropdown-item").click(addToCart);
 
@@ -11,6 +15,6 @@ function addToCart(){
 }
 
 function changeNumberOfCartItems(){    
-    var numberOfCartItems = cartItems.length + 1;
-    $("#numberOfCartItems").html(numberOfCartItems);
+    var numberOfCartItems = cartItems.length;
+    $("#numberOfCartItems").text(numberOfCartItems);
 }
