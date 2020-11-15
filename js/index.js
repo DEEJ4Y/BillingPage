15b260cd140ca51c6aa109820c203c85f04ca16c
davidjosephind <davidjosephind@gmail.com>
2020-11-15 12:41:04 +0530

    added remove functionality
    even though there's some bug. Need to fix.

--- js/index.js
+++ js/index.js
@@ -1,97 +1,108 @@
-var shopItems = [["fridge", "Fridge", 109000], ["desktop", "Desktop PC", 49000], ["laptop", "Laptop", 79000], ["microwave", "Microwave", 19000], ["oven", "Oven", 89000]];
-var cardDiscounts = [["Axis Bank", 2], ["SBI", 4], ["HDFC", 10], ["ICICI", 8], ["Bank of Baroda", 6]];
+    var shopItems = [["fridge", "Fridge", 109000], ["desktop", "Desktop PC", 49000], ["laptop", "Laptop", 79000], ["microwave", "Microwave", 19000], ["oven", "Oven", 89000]];
+    var cardDiscounts = [["Axis Bank", 2], ["SBI", 4], ["HDFC Bank", 10], ["ICICI Bank", 8], ["Bank of Baroda", 6]];
 
-$("#selectCardMenu").hide();
+    var cartItems = [];
+    var totalCost;
+    var chosePaymentMethod = false;
+    var paymentMethod;
+    var choseCardType = false;
+    var chosenCard;
 
-for(var i = 0; i < shopItems.length; i++){
-    $(".dropdownItems").before("<div class='dropdown-item' id='" + shopItems[i][0] + "'><span class='appliance'>" + shopItems[i][1] + "</span><span class='price'>" + shopItems[i][2] + "</span></div>");
-}
+    for(var i = 0; i < shopItems.length; i++){
+        $(".dropdownItems").before("<div class='dropdown-item' id='" + shopItems[i][0] + "'><span class='appliance'>" + shopItems[i][1] + "</span><span class='price'>" + shopItems[i][2] + "</span></div>");
+    }
 
-var cartItems = [];
-var totalCost;
-var chosePaymentMethod = false;
-var paymentMethod;
-var choseCardType = false;
-var chosenCard;
+    function addToCart(){
+        var itemID = $(this).attr("id");
+        cartItems.push(itemID);
+        updateCartDetails();
+        displayAddedItem(itemID);
+    }
 
-$(".chooseAppliance .dropdown-item").click(addToCart);
-
-$(".selectPaymentMethodMenu .dropdown-item").click(selectPaymentMethod);
-
-$(".selectCardDropdownMenu .dropdown-item").click(selectCard);
-
-$(".checkout-button").click(checkout);
-
-
-function addToCart(){
-    var itemID = $(this).attr("id");
-    cartItems.push(itemID);
-    updateCartDetails();
-    displayAddedItem(itemID);
-}
-
-function updateCartDetails(){    
-    var numberOfCartItems = cartItems.length;
-    $("#numberOfCartItems").text(numberOfCartItems);
-    totalCost = 0;    
-    for(var i = 0; i < numberOfCartItems; i++){
-        for(var j = 0; j < shopItems.length; j++){
-            if(cartItems[i] == shopItems[j][0]){
-                totalCost = totalCost + shopItems[j][2];
+    function updateCartDetails(){    
+        var numberOfCartItems = cartItems.length;
+        $("#numberOfCartItems").text(numberOfCartItems);
+        totalCost = 0;    
+        for(var i = 0; i < numberOfCartItems; i++){
+            for(var j = 0; j < shopItems.length; j++){
+                if(cartItems[i] == shopItems[j][0]){
+                    totalCost = totalCost + shopItems[j][2];
+                }
             }
         }
-    }
-    $("#cartItemTotal").text(totalCost);
-    if(numberOfCartItems > 4){
-        $(".cart-item-container").addClass("scrollable-menu");
-    }else{
-        $(".cart-item-container").removeClass("scrollable-menu");
-    }
-}
-
-function displayAddedItem(applianceName){
-    var appliance, price;
-    for(var i = 0; i < shopItems.length; i++){
-        if(applianceName == shopItems[i][0]){
-            appliance = shopItems[i][1];
-            price = shopItems[i][2];
+        $("#cartItemTotal").text(totalCost);
+        if(numberOfCartItems > 3){
+            $(".cart-item-container").addClass("scrollable-menu");
+        }else{
+            $(".cart-item-container").removeClass("scrollable-menu");
         }
     }
-    $(".cartItemsBorder").before("<div class='cart-item-row'><span style='float: left;'>" + appliance + "</span><span style='float: right;'>" + price + "</span></div>");
-}
 
-function selectCard(){
-    chosenCard =  $(this).text();
-    $("#selectCardMenu").text(chosenCard);
-    choseCardType = true;
-}
-
-function selectPaymentMethod(){
-    paymentMethod = $(this).text();
-    chosePaymentMethod = true;
-    $("#paymentMethodMenuButton").text(paymentMethod);
-    if(paymentMethod == "Card"){
-        $("#selectCardMenu").show();
-    }else{
-        $("#selectCardMenu").hide();
+    function displayAddedItem(applianceName){
+        var appliance, price;
+        for(var i = 0; i < shopItems.length; i++){
+            if(applianceName == shopItems[i][0]){
+                appliance = shopItems[i][1];
+                price = shopItems[i][2];
+            }
+        }
+        $(".cartItemsBorder").before("<div class='cart-item-row'><span style='float: left; margin-right= 2rem;'>" + cartItems.length + ". " + appliance + "</span><span style='float: right;'>" + price + " <button class='btn btn-sm btn-outline-danger remove-button' style='float: right;z-index=-10' id='remove-button-" + cartItems.length + "'>Remove</button></span> </div>");
+        $(".remove-button").click(removeFromCart);
     }
-}
 
-function checkout(){
-    if(cartItems.length === 0){
-        alert("There are no items in your cart!");
-    }else if(chosePaymentMethod != true){
-        alert("Please choose a payment method");
-    }else if(paymentMethod == "Card" && choseCardType != true){
-        alert("Please choose your card.");
-    }else{
-        sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
-        sessionStorage.setItem("totalCost", totalCost);
-        sessionStorage.setItem("paymentMethod", paymentMethod);
+    function removeFromCart(){
+        var cartPosition = ($(this).attr("id")).slice(14, ($(this).attr("id")).length);
+        console.log(cartPosition);
+        $(".cart-item-row:contains('"+ cartPosition +".')").hide();
+        for(var i = cartPosition - 1; i < cartItems.length - 1; i++){
+            cartItems[i]=cartItems[i+1];
+        }
+        cartItems.pop();
+        updateCartDetails();
+    }
+
+    function selectCard(){
+        chosenCard =  $(this).text();
+        $("#selectCardMenu").text(chosenCard);
+        choseCardType = true;
+    }
+
+    function selectPaymentMethod(){
+        paymentMethod = $(this).text();
+        chosePaymentMethod = true;
+        $("#paymentMethodMenuButton").text(paymentMethod);
         if(paymentMethod == "Card"){
-            sessionStorage.setItem("chosenCard", chosenCard);
-            sessionStorage.setItem("cardDiscounts", JSON.stringify(cardDiscounts));
+            $("#selectCardMenu").show();
+        }else{
+            $("#selectCardMenu").hide();
         }
-        window.location.href = "../billingPage.html";
-    }    
-}
+    }
+
+    function checkout(){
+        if(cartItems.length === 0){
+            alert("There are no items in your cart!");
+        }else if(chosePaymentMethod != true){
+            alert("Please choose a payment method");
+        }else if(paymentMethod == "Card" && choseCardType != true){
+            alert("Please choose your card.");
+        }else{
+            sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
+            sessionStorage.setItem("totalCost", totalCost);
+            sessionStorage.setItem("paymentMethod", paymentMethod);
+            if(paymentMethod == "Card"){
+                sessionStorage.setItem("chosenCard", chosenCard);
+                sessionStorage.setItem("cardDiscounts", JSON.stringify(cardDiscounts));
+            }
+            window.location.href = "../billingPage.html";
+        }    
+    }
+
+    $("#selectCardMenu").hide();
+
+    $(".chooseAppliance .dropdown-item").click(addToCart);
+
+    $(".selectPaymentMethodMenu .dropdown-item").click(selectPaymentMethod);
+
+    $(".selectCardDropdownMenu .dropdown-item").click(selectCard);
+
+    $(".checkout-button").click(checkout);
